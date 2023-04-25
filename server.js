5f3e6d50aabfddb3fd2ca113f4d564a5b0296c0e
Moh88CS <mohamed.abdulal88@gmail.com>
2023-04-25 18:59:52 -0400

    Implement API methods

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ import express from 'express';
 import { rps, rpsls } from './lib/rpsls.js';
 
 const app = express();
+const argv = minimist(process.argv.slice(2));
 const DEFAULT_PORT = 5555;
-const port = process.argv.includes('--port') ? process.argv[process.argv.indexOf('--port') + 1] : DEFAULT_PORT;
+const port = argv.port || 5555;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -37,6 +38,4 @@ app.use((req, res) => {
   res.status(404).send('404 NOT FOUND');
 });
 
-app.listen(5555, () => {
-    console.log('Server is running on port 5555');
-  });
+app.listen(port)
